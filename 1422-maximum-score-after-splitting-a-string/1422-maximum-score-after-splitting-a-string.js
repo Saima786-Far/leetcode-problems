f18d2ed438fbf07334acef929bf00a4264717c5f
Saima786-Far <84591699+Saima786-Far@users.noreply.github.com>
2023-12-22 22:57:37 +0530

    Time: 60 ms (54.55%), Space: 42 MB (94.55%) - LeetHub

---
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxScore = function(s) {
+    let ans = 0;
+
+    for (let i = 0; i < s.length - 1; i++) {
+        let curr = 0;
+
+        for (let j = 0; j <= i; j++) {
+            if (s.charAt(j) === '0') {
+                curr++;
+            }
+        }
+
+        for (let j = i + 1; j < s.length; j++) {
+            if (s.charAt(j) === '1') {
+                curr++;
+            }
+        }
+
+        ans = Math.max(ans, curr);
+    }
+
+    return ans;
+};
