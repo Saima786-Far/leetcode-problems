dc7d917d2bfab909a3689720d3f71e82c80535e4
Saima Farheen <84591699+Saima786-Far@users.noreply.github.com>
2024-10-05 01:09:24 +0530

    Time: 1 ms (100.00%), Space: 44.2 MB (71.05%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -15,7 +15,7 @@ class Solution {
          int carry = 0;
          while(p!= null || q!= null){
             int x = (p!= null ? p.val :0);
-           int y = (q != null ? q.val : 0);
+            int y = (q!= null ? q.val: 0);
             int sum = carry + x + y;
             carry = sum /10;
             curr.next = new ListNode(sum % 10);
